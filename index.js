d0a48d4a6878ba13621a30656010f4bcbbdda5ba
tsh235 <tanyashustrova@yandex.ru>
2024-01-05 22:05:38 +0300

    переделала через debounce

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const paragraph = document.createElement('p');
 
 document.body.append(input, paragraph);
 
-let timeout = null;
+// let timeout = null;
 
-input.addEventListener('input', () => {
-  clearTimeout(timeout);
+// input.addEventListener('input', () => {
+//   clearTimeout(timeout);
 
-  timeout = setTimeout(() => {
-    paragraph.innerHTML = input.value;
-  }, 300);
-});
+//   timeout = setTimeout(() => {
+//     paragraph.innerHTML = input.value;
+//   }, 300);
+// });
 
+const debounce = (callback, delay) => {
+  let timeout;
+  return () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, delay);
+  };
+};
+
+const printText = () => {
+  paragraph.textContent = input.value;
+};
+
+input.addEventListener('keyup', debounce(printText, 300));
